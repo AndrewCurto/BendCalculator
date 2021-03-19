3844511fa6bf39eaf899a88cbf69fbc4639455e4
Andrew Curto <79021809+AndrewCurto@users.noreply.github.com>
2021-03-18 20:07:29 -0400

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -26,13 +26,17 @@ resultButton.addEventListener("click", function() {
 
 
 
-
-  if (thkInput.value > 1) {
+  if (Number(thkInput.value) > 1) {
     let thkIn = [7, 10, 11, 12, 13, 14, 16, 18, 19, 20, 22, 24, 26, 28];
+console.log(thkIn);
     let thkOut = [.1793, .1345, .1196, .1046, .0897, .0747, .0598, .0478, .0418, .0359, .0299, .0239, .0179, .0149];
-    let inPosition = thkIn.indexOf(thkInput);
-    thkInput = (thkOut[inPosition]);
+console.log(thkOut);    
+    let inPosition = thkIn.indexOf(Number(thkInput.value));
+console.log(inPosition);
+     thkInput.value = (thkOut[inPosition]);
+console.log(thkInput.value);
       }
+
 
   var sumBends = Number(document.getElementById("bend1").value) +
     Number(document.getElementById("bend2").value) +
